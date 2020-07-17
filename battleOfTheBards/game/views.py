ad545d033ba32639876d49c5bd5514f4a4ae7bb2
RebeccaRoach <rebeccaroach12@gmail.com>
2020-07-17 15:00:39 -0700

    Using ModelViewSets instead of generics

--- battleOfTheBards/game/views.py
+++ battleOfTheBards/game/views.py
@@ -4,26 +4,40 @@ from game.models import Poem, Question, Clue
 from game.serializers import PoemSerializer, QuestionSerializer, ClueSerializer
 from rest_framework import generics
 
-class PoemList(generics.ListCreateAPIView):
-    queryset = Poem.objects.all()
-    serializer_class = PoemSerializer
+from rest_framework import viewsets
 
-class PoemDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Poem.objects.all()
-    serializer_class = PoemSerializer
+class PoemViewSet(viewsets.ModelViewSet):
+  queryset = Poem.objects.all()
+  serializer_class = PoemSerializer
 
-class QuestionList(generics.ListCreateAPIView):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
+class QuestionViewSet(viewsets.ModelViewSet):
+  queryset = Question.objects.all()
+  serializer_class = QuestionSerializer
 
-class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
+class ClueViewSet(viewsets.ModelViewSet):
+  queryset = Clue.objects.all()
+  serializer_class = ClueSerializer
 
-class ClueList(generics.ListCreateAPIView):
-    queryset = Clue.objects.all()
-    serializer_class = ClueSerializer
+# class PoemList(generics.ListCreateAPIView):
+#     queryset = Poem.objects.all()
+#     serializer_class = PoemSerializer
 
-class ClueDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Clue.objects.all()
-    serializer_class = ClueSerializer
+# class PoemDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Poem.objects.all()
+#     serializer_class = PoemSerializer
+
+# class QuestionList(generics.ListCreateAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = QuestionSerializer
+
+# class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = QuestionSerializer
+
+# class ClueList(generics.ListCreateAPIView):
+#     queryset = Clue.objects.all()
+#     serializer_class = ClueSerializer
+
+# class ClueDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Clue.objects.all()
+#     serializer_class = ClueSerializer
