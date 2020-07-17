679d0a4feffbbbd8fc794793d87b82684b5c2b0d
RebeccaRoach <rebeccaroach12@gmail.com>
2020-07-16 21:52:21 -0700

    Wrote generic views for classes and wired up urls

--- battleOfTheBards/game/urls.py
+++ battleOfTheBards/game/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path
-
-from . import views
+# from rest_framework.urlpatterns import format_suffix_patterns
+from game import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
+    # need to define an index view?
+    # path('', views.index, name='index'),
+    path('poems/', views.PoemList.as_view()),
+    path('poems/<int:pk>/', views.PoemDetail.as_view()),
+    path('questions/', views.QuestionList.as_view()),
+    path('questions/<int:pk>/', views.QuestionDetail.as_view()),
+    path('clues/', views.ClueList.as_view()),
+    path('clues/<int:pk>/', views.ClueDetail.as_view()),
 ]
+
+# from django.urls import include, path
+# from rest_framework import routers
+# from . import views
+
+# router = routers.DefaultRouter()
+# router.register(r'heroes', views.HeroViewSet)
+
+# # Wire up our API using automatic URL routing.
+# # Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
--- battleOfTheBards/game/views.py
+++ battleOfTheBards/game/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(request):
-  return HttpResponse("Hello, world! Game index!")
+from game.models import Poem, Question, Clue
+from game.serializers import PoemSerializer, QuestionSerializer, ClueSerializer
+from rest_framework import generics
+
+class PoemList(generics.ListCreateAPIView):
+    queryset = Poem.objects.all()
+    serializer_class = PoemSerializer
+
+class PoemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Poem.objects.all()
+    serializer_class = PoemSerializer
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class ClueList(generics.ListCreateAPIView):
+    queryset = Clue.objects.all()
+    serializer_class = ClueSerializer
+
+class ClueDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Clue.objects.all()
+    serializer_class = ClueSerializer
