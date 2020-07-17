0494e4e355904a3d452e208feb0b100fe8137337
RebeccaRoach <rebeccaroach12@gmail.com>
2020-07-17 14:59:34 -0700

    Registered routers for default routing

--- battleOfTheBards/game/script.py
+++ battleOfTheBards/game/script.py
@@ -4,18 +4,18 @@ from game.models import Poem, Question, Clue
 with open('game/poem_data.json', 'r') as json_file:
   poem_data = json.loads(json_file.read())
   for p in poem_data:
-    questions = p.pop("questions")
+    # questions = p.pop("questions")
     poem = Poem(**p)
     poem.save()
 
-    for q in questions:
-      q['poem_id'] = poem.id
-      clues = q.pop("clues")
-      question = Question(**q)
-      question.save()
+    # for q in questions:
+    #   q['poem_id'] = poem.id
+    #   clues = q.pop("clues")
+    #   question = Question(**q)
+    #   question.save()
 
-      for c in clues:
-        c['question_id'] = question.id
-        clue = Clue(**c)
-        clue.save()
+    #   for c in clues:
+    #     c['question_id'] = question.id
+    #     clue = Clue(**c)
+    #     clue.save()
   
--- battleOfTheBards/game/urls.py
+++ battleOfTheBards/game/urls.py
@@ -2,27 +2,29 @@ from django.urls import path
 # from rest_framework.urlpatterns import format_suffix_patterns
 from game import views
 
-urlpatterns = [
-    # need to define an index view?
-    # path('', views.index, name='index'),
-    path('poems/', views.PoemList.as_view()),
-    path('poems/<int:pk>/', views.PoemDetail.as_view()),
-    path('questions/', views.QuestionList.as_view()),
-    path('questions/<int:pk>/', views.QuestionDetail.as_view()),
-    path('clues/', views.ClueList.as_view()),
-    path('clues/<int:pk>/', views.ClueDetail.as_view()),
-]
+# urlpatterns = [
+#     # need to define an index view?
+#     # path('', views.index, name='index'),
+#     path('poems/', views.PoemList.as_view()),
+#     path('poems/<int:pk>/', views.PoemDetail.as_view()),
+#     path('questions/', views.QuestionList.as_view()),
+#     path('questions/<int:pk>/', views.QuestionDetail.as_view()),
+#     path('clues/', views.ClueList.as_view()),
+#     path('clues/<int:pk>/', views.ClueDetail.as_view()),
+# ]
 
-# from django.urls import include, path
-# from rest_framework import routers
+from django.urls import include, path
+from rest_framework import routers
 # from . import views
 
-# router = routers.DefaultRouter()
-# router.register(r'heroes', views.HeroViewSet)
+router = routers.DefaultRouter()
+router.register(r'poems', views.PoemViewSet)
+router.register(r'questions', views.QuestionViewSet)
+router.register(r'clues', views.ClueViewSet)
 
-# # Wire up our API using automatic URL routing.
-# # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
