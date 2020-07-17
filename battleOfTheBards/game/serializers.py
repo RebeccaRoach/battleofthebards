b9dbf1b166346f100dd4c066d2b61abe193d7048
RebeccaRoach <rebeccaroach12@gmail.com>
2020-07-16 21:51:25 -0700

    Wrote class serializers to inherit from ModelSerializer

--- battleOfTheBards/game/serializers.py
+++ battleOfTheBards/game/serializers.py
@@ -1,46 +1,24 @@
-# serializing means turning python objects into JSON
-# deserializing is the opposite - need to do this first
-# check for where to write script to process poem json
-
-
-#  has many relationships specified here
 from rest_framework import serializers
-from game.models import Poem, Question, Clue, Answer
+from game.models import Poem, Question, Clue
 
-class QuestionSerializer(serializers.Serializer):
-    # id = serializers.IntegerField(read_only=True)
+# TODO: USE HYPERLINKED Model Serializers to reflect relationships between entities in browsable API
+# class HeroSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Hero
+#         fields = ('name', 'alias')
 
-    # poem = models.ForeignKey(Poem, on_delete=models.CASCADE)
-    # text = models.CharField(max_length=1000)
-    # point_value = models.IntegerField(default=0)
-    # answered_correctly = models.BooleanField(default=False)
+class QuestionSerializer(serializers.ModelSerializer):
+  class Meta:
+   model = Question
+   # grabs all the fields from model
+   exclude = []
 
-    poem = serializers.(Poem, on_delete=models.CASCADE)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    point_value = serializers.IntegerField
-    answered_correctly = serializers.BooleanField
-    text = serializers.ChoiceField(required=True)
-    # style={'base_template': 'textarea.html'}
+class PoemSerializer(serializers.ModelSerializer):
+  class Meta:
+   model = Poem
+   exclude = []
 
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
-
-# postgres db
-# write seed script somewhere to json -> models in DB
-# serializers to write and play with
-# deployment 
+class ClueSerializer(serializers.ModelSerializer):
+  class Meta:
+   model = Clue
+   exclude = []
